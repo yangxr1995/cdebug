38b0ce67d1769579d84f73551cc932b55760ff81
root <root@DESKTOP-I2GJKOV.localdomain>
2025-09-17 00:35:56 +0800

    初步实现地址合并

--- parse_addr_log/main.cc
+++ parse_addr_log/main.cc
@@ -8,12 +8,16 @@
 #include <getopt.h>
 #include <cstring>
 #include <iostream>
+#include <map>
 #include <sys/types.h>
 #include <unistd.h>
 #include <string>
 #include <unordered_map>
 #include <utility>
 #include <vector>
+
+int gDelCount;
+int gDelLineNb;
 
 class InfoBlock {
     public:
@@ -52,9 +56,10 @@ class InfoBlock {
         }
 
         bool operator==(const InfoBlock &infoBlock) {
-            return type_ == infoBlock.type_ 
-                // && dataHashVal_ == infoBlock.dataHashVal_
-                && data_ == infoBlock.data_;
+            return data_ == infoBlock.data_;
+            // return type_ == infoBlock.type_ 
+            //     && dataHashVal_ == infoBlock.dataHashVal_
+            //     && data_ == infoBlock.data_;
         }
 
         bool operator!=(const InfoBlock &infoBlock) {
@@ -65,11 +70,13 @@ class InfoBlock {
             pos_ = std::move(pos);
         }
 
-        void setMPos(std::unordered_multimap<std::string, InfoBlock *>::iterator mpos) {
+        // void setMPos(std::unordered_multimap<std::string, InfoBlock *>::iterator mpos) {
+        void setMPos(std::multimap<std::string, InfoBlock *>::iterator mpos) {
             mpos_ = std::move(mpos);
         }
 
-        const std::unordered_multimap<std::string, InfoBlock *>::iterator & mPos() const {
+        // const std::unordered_multimap<std::string, InfoBlock *>::iterator & mPos() const {
+        const std::multimap<std::string, InfoBlock *>::iterator & mPos() const {
             return mpos_;
         }
 
@@ -111,7 +118,8 @@ class InfoBlock {
         InfoBlockType type_;
         std::string data_;
         std::list<InfoBlock*>::iterator pos_;
-        std::unordered_multimap<std::string, InfoBlock *>::iterator mpos_;
+        // std::unordered_multimap<std::string, InfoBlock *>::iterator mpos_;
+        std::multimap<std::string, InfoBlock *>::iterator mpos_;
         uint32_t dataHashVal_;
         uint32_t repeatCount_;
         std::string mapKey_;
@@ -166,16 +174,24 @@ int main (int argc, char *argv[]) {
     InfoBlock::InfoBlockType type = InfoBlock::kInfoBlockData;
 
     std::list<InfoBlock *> infoBlockList;
-    std::unordered_multimap<std::string, InfoBlock *> infoBlockMap;
+    // std::unordered_multimap<std::string, InfoBlock *> infoBlockMap;
+    std::multimap<std::string, InfoBlock *> infoBlockMap;
 
-    int minEqualNb = 1;
+    int minEqualNb = 3;
 
     // load file
-    while(!isEnd) {
+    while(1) {
         std::getline(inFile, line);
 
         if (inFile.eof()) {
-            isEnd = true;
+            if (b) {
+                infoBlockList.emplace_back(b);
+                auto it = infoBlockList.end();
+                --it;
+                b->setPos(it);
+                b->line_ = lineNb;
+            }
+            break;
         }
         else {
             lineNb++;
@@ -217,13 +233,14 @@ int main (int argc, char *argv[]) {
 
     }
 
-    std::cout << "load file : ok" << std::endl;
 
+    auto itList = infoBlockList.end();
+    --itList;
     // map
     for (auto itList = infoBlockList.begin(); itList != infoBlockList.end(); ++itList) {
         int n = std::distance(itList, infoBlockList.end());
         if (n < minEqualNb) {
-            std::cout << __LINE__ << ": " << n << " < " << minEqualNb << std::endl;
+            // std::cout << __LINE__ << ": " << n << " < " << minEqualNb << std::endl;
             continue;
         }
 
@@ -266,137 +283,77 @@ __next__:
 
         for (; itList != infoBlockList.end(); ++itList) {
 
+            // 末尾节点没有mapKey
             if ((*itList)->mapKey().empty()) {
                 continue;
             }
 
             auto range = infoBlockMap.equal_range((*itList)->mapKey());
-
-            int seq = std::distance(infoBlockList.begin(), itList);
-
-            int rangeLen = std::distance(range.first, range.second) + 1;
-
-            if (rangeLen == 1)
-                continue;
-
-            std::cout << "infoBlockList size: " << infoBlockList.size() << std::endl;
-            std::cout << "infoBlockMap size: " << infoBlockMap.size() << std::endl;
-            std::cout << "seq: " << seq << ", rangeLen: " << rangeLen << std::endl;
+            int itListIndex = std::distance(infoBlockList.begin(), itList);
+            std::cout << "itListIndex : " <<  itListIndex  << ", range size : " << std::distance(range.first, range.second) << std::endl;
 
             for (auto itMap = range.first; itMap != range.second; ) {
-                if ((*itList) == itMap->second) {
-                    ++itMap;
-                    // itMap = infoBlockMap.erase(itMap);
-                    continue;
-                }
+                InfoBlock *pInfoBlock = itMap->second;
 
-                std::cout << "target key : " << (*itList)->mapKey() << std::endl;
-                std::cout << std::distance(infoBlockList.begin(), itList) << " cmp "<< std::distance(infoBlockList.begin(), itMap->second->pos()) << std::endl;
-
-                // std::cout << "curMap key : " << itMap->second->mapKey() << std::endl;
-                // if ((*itList)->mapKey() == itMap->second->mapKey()) {
-                //     std::cout << "target key cmp curMap key" << std::endl;
-                //     std::cout << (*itList)->line_ << " != " << itMap->second->line_ << std::endl;
-                // }
-                // getchar();
-
-                std::list<InfoBlock *>::iterator itList2;
-                itList2 = (itMap->second)->pos();
-
-                int n1 = std::distance(infoBlockList.begin(), itList);
-                int n2 = std::distance(infoBlockList.begin(), itList2);
-
-                int n = n2 - n1 - 1;
-                if (n < 0) {
+                if ((*itList)->line_ >= pInfoBlock->line_) {
                     itMap = infoBlockMap.erase(itMap);
-                    printf("%s %d\n", __PRETTY_FUNCTION__, __LINE__);
+                    // std::cout << "continue " << __LINE__ << std::endl;
                     continue;
                 }
 
-                if (n  > minEqualNb + 50) {
+                int itListIndex2 = std::distance(infoBlockList.begin(), pInfoBlock->pos());
+                int n = itListIndex2 - itListIndex;
+
+                if (n < minEqualNb) {
                     ++itMap;
                     continue;
                 }
 
-                if (std::distance(itList2, infoBlockList.end()) - 1 < n) {
-                    printf("%s %d\n", __PRETTY_FUNCTION__, __LINE__);
+                if (n > 100) {
+                    break;
+                }
+
+                // std::cout << "itListIndex2 - itListIndex : " << n << std::endl;
+
+                if (std::distance(pInfoBlock->pos(), infoBlockList.end()) < n) {
+                    // std::cout << "continue " << __LINE__ << std::endl;
                     ++itMap;
                     continue;
                 }
 
-                int i;
-                auto itListPos = std::next(itList, n);
-                auto itList2Pos = std::next(itList2, n);
-
-                for (i = 1; i < n; ++i, --itListPos, --itList2Pos) {
-                    if (**itListPos != **itList2Pos)
+                auto pos1 = std::next(itList, n - 1);
+                auto pos2 = std::next(pInfoBlock->pos(), n - 1);
+                size_t i;
+                for (i = 0; i < n; i++, --pos1, --pos2) {
+                    if ((*pos1)->data() != (*pos2)->data()) {
                         break;
+                    }
                 }
 
-                if (i < n) {
-                    std::cout << "i[" << i << "], n[" << n << "]" << std::endl;
-                    printf("%s %d\n", __PRETTY_FUNCTION__, __LINE__);
-                    ++itMap;
-                    continue;
-                }
+                if (i == n) {
+                    gDelCount++;
+                    // std::cout << "find equal at " << (*itList)->line_ << std::endl;
 
-                // std::string tmp1, tmp2;
-                // itListPos = itList;
-                // itList2Pos = itList2;
-                // for (int j = 0; j < n ; ++j, ++itListPos) {
-                //     tmp1 += (*itListPos)->data();
-                // }
-                //
-                // for (int j = 0; j < n ; ++j, ++itList2Pos) {
-                //     tmp2 += (*itList2Pos)->data();
-                // }
+                    auto pos = std::next(itList, n - 1);
+                    (*pos)->addRepeatCount();
 
-                // std::cout << "find equal : " << std::endl;
-                // std::cout << tmp1 << std::endl;
-                // std::cout << "-----------------------" << std::endl;
-                // std::cout << tmp2 << std::endl;
-                //
-                // std::cout << "+++++++++++++++++++++++" << std::endl;
-                //
-                // std::cout << "del " << n << std::endl;
-                // getchar();
+                    pos = pInfoBlock->pos();
 
-                // del [itList2, itList2 + n)
-                {
-                    auto pos = itList2;
-                    --pos;
-                    auto lastPos = pos;
-                    ++pos;
-                    for (int m = 0; m < n; ++m) {
+                    // std::cout << "find equal2 at " << (*pos)->line_ << std::endl;
 
-
+                    for (int i = 0; i < n; ++i) {
+                        // std::cout << "del " << (*pos)->line_ << std::endl;
                         infoBlockMap.erase((*pos)->mPos());
-                        // auto range = infoBlockMap.equal_range((*pos)->data());
-                        // for (auto it = range.first; it != range.second; ) {
-                        //     if (it->second == *pos) {
-                        //         it = infoBlockMap.erase(it);
-                        //         break;
-                        //     }
-                        //     else {
-                        //         ++it;
-                        //     }
-                        // }
-
-                        std::cout << "del :" << (*pos)->line_ << std::endl;
-                        if ((*pos)->repeatCount()) {
-                            (*lastPos)->addRepeatCount((*pos)->repeatCount());
-                        }
-
                         pos = infoBlockList.erase(pos);
+                        gDelLineNb++;
                     }
 
-                    (*lastPos)->addRepeatCount();
-
                     hintBegin = itList;
-                    // hintBegin = pos;
 
                     goto __next__;
-
+                }
+                else {
+                    // std::cout << "continue " << __LINE__ << std::endl;
                 }
 
                 ++itMap;
@@ -406,6 +363,9 @@ __next__:
 
         break;
     }
+
+    std::cout << "gDelCount : " << gDelCount << std::endl;
+    std::cout << "gDelLineNb : " << gDelLineNb * 3 << std::endl;
 
     std::ofstream outFile(outFileName);
 
@@ -418,7 +378,7 @@ __next__:
         outFile.write(it->data().c_str(), it->data().length());
         if (it->repeatCount()) {
             char buf[32];
-            snprintf(buf, sizeof(buf), "repeat %d\n", it->repeatCount());
+            snprintf(buf, sizeof(buf), "repeat ...\n");
             outFile.write(buf, strlen(buf));
         }
     }
